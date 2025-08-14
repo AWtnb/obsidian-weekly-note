8cc19187ee7b67558de83581fe85ceb540196a53
awtnb <arikaebanataw@gmail.com>
2025-08-14 22:08:51 +0900

    update readme

---
+++ install.ps1
@@ -0,0 +1,29 @@
+function Copy-PluginFiles {
+    param(
+        [string]$vaultDir
+    )
+    if (-not (Test-Path $vaultDir -PathType Container)) {
+        "Vault path not found on this computer." | Write-Host -ForegroundColor Red
+        return
+    }
+
+    $extensionDir = $vaultDir | Join-Path -ChildPath ".obsidian\plugins\obsidian-weekly-note"
+    if (-not (Test-Path $extensionDir -PathType Container)) {
+        New-Item $extensionDir -ItemType Directory
+    }
+    Get-ChildItem $PSScriptRoot | Where-Object {$_.Name -in @("main.js", "styles.css", "manifest.json")} | Copy-Item -Destination $extensionDir
+}
+
+try {
+    Get-Command npm -ErrorAction stop > $null
+    npm install
+    if ($LASTEXITCODE -eq 0) {
+        npm run build
+        if ($LASTEXITCODE -eq 0) {
+            Copy-PluginFiles -vaultDir $args[0]
+        }
+    }
+}
+catch {
+    Write-Error $_
+}
